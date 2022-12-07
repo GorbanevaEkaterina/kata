619863f0011f77b4b86320e70f7c7b804752bf02
GorbanevaEkaterina <105439408+GorbanevaEkaterina@users.noreply.github.com>
2022-12-07 16:59:35 +0400

    task2

---
+++ index.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
