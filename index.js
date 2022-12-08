8d57c1fa479f48ee6599a25013b80a9fc78b4a49
GorbanevaEkaterina <105439408+GorbanevaEkaterina@users.noreply.github.com>
2022-12-08 18:00:34 +0400

    task2

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ let swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
-    },
+    }
+});
+
+const button = document.querySelector('.footer__more_button');
+const footer__button = document.querySelector('.footer__brand_button')
+button.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    footer__button.classList.toggle('.button__more');
 });
