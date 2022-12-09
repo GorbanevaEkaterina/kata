153428329d9448809a944a2fc2519b559345e5ee
GorbanevaEkaterina <105439408+GorbanevaEkaterina@users.noreply.github.com>
2022-12-09 20:11:54 +0400

    Всё починила.

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ let swiper = new Swiper(".mySwiper", {
     }
 });
 
+
+
 const button = document.querySelector('.footer__more_button');
-const footer__button = document.querySelector('.hidden')
+const footer__button = document.querySelector('.hidden');
+const arrow__button = document.querySelector('.more__expand');
+const arrow__buttonn = document.querySelector('.more__expand_active');
 button.addEventListener('click', (e)=> {
     footer__button.classList.toggle('button__more');
+    arrow__button.classList.toggle('see');
+    arrow__buttonn.classList.toggle('seee');
     if (button.innerText === "Показать всё") {
+        arrow__button.style.display = 'none';
         button.innerText = "Скрыть";
     } else {
         button.innerText = "Показать всё";
+        arrow__button.style.display = 'block';
     }
 });
-
-
